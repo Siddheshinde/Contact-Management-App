9fe342d06e2f7e686305550e55d0a95542aedfbc
Siddheshinde <siddhesh19205@gmail.com>
2026-01-06 22:49:04 +0530

    utils code

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import { use } from "react"
+
+const app= express()
+
+app.cora(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export{app}
