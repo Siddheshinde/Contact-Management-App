1b685dbe56f5bc42997938316dcbcfb61cf9fd35
Siddheshinde <siddhesh19205@gmail.com>
2026-01-09 23:18:27 +0530

    Some minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "http://localhost:8000/api/v1/contacts";
+const API_URL = "https://contact-backend.onrender.com/api/v1/contacts";
+
 
 function App() {
   const [contacts, setContacts] = useState([]);
